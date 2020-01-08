6a8a5cc197ebc2ee9c516b62c25d6417805a96fc
Like Sha <tradersalike88@yahoo.com>
2020-01-08 15:57:40 -0800

    officehour01_08_2020

---
+++ src/office_hour/Practic_01_08_2020_task2_task3.java
@@ -0,0 +1,67 @@
+package office_hour;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Practic_01_08_2020_task2_task3 {
+
+    public static void main(String[] args) {
+
+        /**
+         * Given a ArrayList of 10 String(yes String) as scores "79" , "54" , "100", "65", "44"....
+         * * print pass if its more than 60 fail if not
+         * * count how many score are more than 90
+         * * get the average score
+         * * update the values with score-grade pair
+         *   for example
+         * 79C , 54F, 100A, 65D, 44F , 89B , 95A
+         * Optionally create 4 methods according to above criteria
+         *
+         *
+         */
+         List<String> numLst = Arrays.asList("79","54","100","65","44","78","68","98","89","77");
+        System.out.println("numLst = " + numLst);
+          // print pass if its more than 60 fail if not
+        for(String each : numLst){
+
+            int eachNum = Integer.parseInt(each);
+            if(eachNum > 60){
+                System.out.println("pass = " + eachNum);
+            }else{
+                System.out.println("Fail = " + eachNum);
+            }
+
+        }
+
+          // count how many score are more than 90
+        int count = 0 ;
+        for(String each : numLst){
+
+            if(Integer.parseInt(each)> 90){
+                ++count ;
+            }
+
+        }
+        System.out.println("score more than 90 count is = " + count);
+
+        // get the average score
+        int sum = 0 ;
+        for(String each : numLst){
+
+            sum = sum + Integer.parseInt(each);
+
+        }
+        System.out.println("sum = " + sum);
+        System.out.println("average = " + sum / numLst.size());
+
+        //update the values with score-grade pair
+        //for example
+        //         * 79C , 54F, 100A, 65D, 44F , 89B , 95A
+
+        // logic : go through each and every number
+        // check the number for the range value so we can decide it's A,B,C,D,F
+        // Then use the set method to update the current value to new value
+        numLst.set(0,numLst.get(0)+ "C");
+        // TODO : finish the rest using
+    }
+}
