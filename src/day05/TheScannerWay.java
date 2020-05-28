7d600da11ac5d1095bb407b9168292c75db69d5a
Like Sha <tradersalike88@yahoo.com>
2020-05-27 23:41:08 -0700

    add some classNotes in ScannerWay class

--- src/day05/TheScannerWay.java
+++ src/day05/TheScannerWay.java
@@ -6,6 +6,15 @@ public class TheScannerWay {
 
     public static void main(String[] args) {
 
+        // Scanner is class that comes from JDK library ,We can use it to capture keyboard input
+        // from users
+
+        //In order to use Scanner we need to first import it as below
+        // Import java.util.Scanner;
+        // The import statement must be in between your package declaration and before the
+        //beginning of the class
+
+
         // ask user for name
         // and capture the result
         // print your name is this
@@ -21,6 +30,7 @@ public class TheScannerWay {
         // you can name your variable whatever you want
         // in below case we are calling it scan
         // Scanner is data type, scan is the name, new Scanner(System.in) is the value
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("What is your name :");
@@ -38,6 +48,15 @@ public class TheScannerWay {
         double height = scan.nextDouble();
         System.out.println("your height is : " + height);
 
+        /*
+        There is few ways to get users input and store the result
+
+        Getting single word as String -->scan.next();
+        Getting single number as int-->scan.nextInt();
+        Getting single fractional number as double -->scan.nextDouble();
+        Getting single fractional number as float --> scan.nextFloat();
+        Getting single true or false as boolean --> scan.nextBoolean();
+         */
 
 
 
