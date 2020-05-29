4abc389840bb0be454bd183e00ba0c4971966a89
Like Sha <tradersalike88@yahoo.com>
2020-05-29 02:36:50 -0700

    spelling errors corrected

--- src/day06/PreIncrementDecrementOperator.java
+++ src/day06/PreIncrementDecrementOperator.java
@@ -10,11 +10,11 @@ public class PreIncrementDecrementOperator {
         // increasing the number by one using normal way ;
         //offerCount = offerCount + 1 ;
 
-        // Since increasing or decreasing a value is v ery special
+        // Since increasing or decreasing a value is very special
         // and often used in programming
         // There is even shorter shortcut for this operation
         // and we use ++ or -- , THIS IS EXCLUSIVELY FOR INCREASING OR DECREASING BY 1
-        // We can NOT use it for any other time like increasing or decreasin value by more than one.
+        // We can NOT use it for any other time like increasing or decreasing value by more than one.
         ++ offerCount ; // this is same as offerCount  +=1 ; and this offerCount  = offerCount + 1 ; it's just
         System.out.println("Offer Now after increasing by one " + offerCount);
 
