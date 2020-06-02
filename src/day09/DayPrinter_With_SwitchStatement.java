41cbae12b1a014aedf244ee3c4c4bb90163dab86
Like Sha <tradersalike88@yahoo.com>
2020-06-02 00:52:12 -0700

    small amendment on classDayPrinterwithswitchstatement class

--- src/day09/DayPrinter_With_SwitchStatement.java
+++ src/day09/DayPrinter_With_SwitchStatement.java
@@ -4,40 +4,65 @@ public class DayPrinter_With_SwitchStatement {
 
     public static void main(String[] args) {
 
-        int dayCode = 2;
+        int dayCode = 1;
 
-        switch(dayCode) {
-            // 1 is the actual value you are checking
-            // just like dayCode == 1
+        switch(dayCode){
             case 1 :
-                System.out.println("Day is Monday");
-                break; // used to get out of this branch
+                System.out.println("It's Monday");
+                break;
             case 2 :
-                System.out.println("Day is Tuesday");
+                System.out.println("It's Tuesday");
                 break ;
             case 3 :
-                System.out.println("Day is Wednesday");
-                break ;
+                System.out.println("It's Wednesday");
+                break;
             case 4 :
-                System.out.println("Day is Thursday");
-                break ;
+                System.out.println("It's Thursday");
+                break;
             case 5 :
-                System.out.println("Day is Friday");
-                break ;
+                System.out.println("It's Friday");
+                break;
             case 6 :
-                System.out.println("Day is Saturday");
+                System.out.println("It's Saturday");
                 break ;
             case 7 :
-                System.out.println("Day is Sunday");
-                break ;
-            default :
-                System.out.println("Day is unknown ");
+                System.out.println("It's Saturday");
+                break;
+            default:
+                System.out.println("Day is UNKNOWN!!!");
+
+
 
 
         }
 
-
-
+//        int dayCode1=1;
+//        switch(dayCode1){
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Unknown day");
+//        }
+//
 
 
     }
