6015c95e11ca3951d9c62591ef42d6d32333d0d6
Like Sha <tradersalike88@yahoo.com>
2020-06-01 23:22:37 -0700

    MultiBranchIfStatement class made a little change

--- src/day08/MultiBranchIfStatement.java
+++ src/day08/MultiBranchIfStatement.java
@@ -15,20 +15,21 @@ public class MultiBranchIfStatement {
         * ----start from here above two optional
         *   more than 70 -->> point taken
         *   more than 60 and less than 70 -->> warning
+        *   if not speeding keep driving
         *
-        * if not speeding keep driving
         *
         *
          */
           // TRY 3 DIFFERENT INPUTS  45, 65, 90 BAD IDEA, CONDITION SHOULD BE ALWAYS WITH TOP TO BOTTOM OR
-          int currentSpeed = 45 ;
 
-          if( currentSpeed>60){
+        int currentSpeed = 80 ;
+
+          if( currentSpeed>70){
               System.out.println(" you are speeding more than 60--POINTS TAKEN !!!");
            // asking whether it's less than or equal 70 and more than 60
               // when you come to this point, your speed is already not more than 70
-          }else if( currentSpeed > 70 ){
-              System.out.println("Your speed is less than  ");
+          }else if( currentSpeed > 60 ){
+              System.out.println("Your speed is less than or equal to 70 but more thant 60");
           }else{
               System.out.println("KEEP DRIVING YOU ARE GOOD !!!");
           }
