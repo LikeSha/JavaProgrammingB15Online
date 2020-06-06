1ee98ed23db9b0d0dd0b04a5229b031def8dbf61
Like Sha <tradersalike88@yahoo.com>
2020-06-06 03:17:17 -0700

    softskill interview questions

---
+++ src/InterviewsCodeKnowledge/InterviewSoftSkillKnowledge.java
@@ -0,0 +1,93 @@
+package InterviewsCodeKnowledge;
+
+public class InterviewSoftSkillKnowledge {
+
+    /**
+     * Soft skill :
+     *
+     *         three amigo meeting : developer, tester and Business Analyst ( BA ), its an informal meeting ,we can do anywhere
+     *         who write the requirement ? BA
+     *
+     *         /How to test the application if you don't have any requirement ? (Interview question)
+     *             // Answer : Ask developer !!! ( only person you can ask for ) tell them " I don't have user story,
+     *             // ask them about functionality and go to test application.
+     *
+     *             Who assign requirements ( user stories) to testers  ? Answer :
+     *             team leader  in developing team, developers assign the user stories to developers ,
+     *             in testing team, test lead assign user stories to testers
+     *
+     *             unit testing done by developers ,and unit testing is white-box testing.
+     *             white-box testing needs code knowledge
+     *
+     *             before testing ,testers need to :
+     *
+     *             1. read requirement
+     *             2, start testing:
+     *                 write test case; (after write test case, you start  manual testing)
+     *
+     *
+     *             minor regression -->> is happened at end of each sprint.
+     *             major regression--> normally every 3 months or every 6 sprint
+     *
+     *             is re-testing same as regression testing ? NO
+     *                       re-testing : fix bugs( specific functionalities fixed )
+     *                       regression testing ( when testing whole production )
+     *
+     *             How often you do your regression ?
+     *                        generally , we do our regression each 3 month or each 6 sprint.
+     *                         ( another saying is less than 4 weeks)
+     *
+     *             // who decide sprint cycle ? scrum master
+     *
+     *             // how's your regression ? our regression is 65% automated and
+     *                     rest of them are done by manual .
+     *                     the reason is not all functionalities can be done by automation ,you have to do manually .
+     *
+     *              our regression normally done by automated and takes about 3-4 hours
+     *
+     *      Unit testing is white box testing because unit test required test code.
+     *
+     *         Ad-Hoc testing is tesing without any plan or documentation. its considered as informal testing .
+     *         its also call random testing or Monkey testing. also called Exploratory testing .
+     *         lots of defects. good to perform ad-hoc testing once official testing is done and still sometimes
+     *         left for testing.
+     *
+     *         Exploratory testing done by tester who dose not know anything about the application.
+     *         the purpose of exploratory testing is : to learn and explore the application.
+     *
+     *         component testing = module testing ,testing each module separately
+     *
+     *         integreation testing tests after integrating mulitiple components to check
+     *         if the system works fine or not.
+     *
+     *         UAT ---User Acceptance Testing  after system test has corrected ,
+     *         the system will delivered to user or customer for acceptance testing ,
+     *         sometimes stakeholders may be involved as well.
+     *
+     *         UAT testing : alpha testing (done by technical people)
+     *                       beta testing (done by client, customer)
+     *
+     *         Interview question :
+     *
+     *         1, what type of testing have you done so far ?( they want to know your test level )
+     *
+     *            My previouse project, I working on functional testing . smoke testing,
+     *            integration testing,system testing ,and regression testing .
+     *            I also did manual and automation testing too.
+     *
+     *             interview questions:
+     *
+     *            How many test cases in your smoke testing ?
+     *            20--30 test cases in my previouse project , and can be done within 30 minutes
+     *
+     *            the purpose of smoke testing :
+     *            to make sure the environment is up and running , make sure the application is stable .
+     *
+     *
+     *            the advantage of smoke testing is :
+     *            1.it will help us quickly to identify the issue .
+     *            2.testing team will not waste their time on perform any type of testing on faulty build.
+     *
+     *
+     */
+}
