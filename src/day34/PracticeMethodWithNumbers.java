644efa270a00ff3ecda00a1e103a772c5fa3e6a9
Like Sha <tradersalike88@yahoo.com>
2020-07-02 22:40:46 -0700

    add some codes notes in Java course day 34

--- src/day34/PracticeMethodWithNumbers.java
+++ src/day34/PracticeMethodWithNumbers.java
@@ -68,4 +68,18 @@ public class PracticeMethodWithNumbers {
      * scan.next();
      */
 
+    /**
+     * printName("Karmran")
+     * printName("Astrit");
+     * // public ----access modifier
+     *    static ---does not an object to call
+     *    void  --- return type , does not return anything
+     *    String name --- method parameter
+     *    "Astrit", "Kamran" the actual data that you passed. is also called " argument"
+     *
+     *    public static void printName(String name){
+     *        System.out.println("Your name is "+ name);
+     *    }
+     */
+
 }
