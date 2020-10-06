4439e5c65788f0b005ac810f1811055da4689e89
Like Sha <tradersalike88@yahoo.com>
2020-10-06 15:55:24 -0700

    java DAY 56 ItemAction codes updates

---
+++ src/day56/replTjMax/ItemAction.java
@@ -0,0 +1,13 @@
+package day56.replTjMax;
+
+public class ItemAction {
+
+    public static void main(String[] args) {
+
+        Item i1 = new OnSaleItem("Cake",1111,2,20.0,50);
+        System.out.println("cake price = " + i1.getPrice());
+        System.out.println(i1);
+
+
+    }
+}
