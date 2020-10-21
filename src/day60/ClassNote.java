684406fb9f9f47c80b6478a948690ee5b43ca0b5
Like Sha <tradersalike88@yahoo.com>
2020-10-20 20:40:20 -0700

    java course day60 Exceptions topic

--- src/day60/ClassNote.java
+++ src/day60/ClassNote.java
@@ -3,6 +3,48 @@ package day60;
 public class ClassNote {
 
 /**
+ * Exception :
+ *    abnormal or non-typical event
+ *    to cause alteration of program flow
+ *
+ *    When exception occur in the program, an specific type of exception object that contains information
+ *    will be created and thrown according to what happen and thrown to runtime system.
+ *
+ *    9/0 --> new ArithmeticException ( some message)
+ *    How do I throw it ?
+ *    throw new ArithmeticException(some message) this is what happened behind the screen.
+ *
+ *    Exceptions are object just like everything else in Java
+ *
+ *    "abc".charAt(100)--> throw new StringIndexOutOfBoundsException(...)
+ *
+ *    Since exceptions are objects, It has its own class to be the blue print for the exception
+ *
+ *    Exceptions are categorized by INHERITANCE RELATIONSHIP
+ *      Exception Hierachy:
+ *                                 Throwable
+ *
+ *               Error                                   Exception
+ *
+ *                                    RuntimeException                       Everything else
+ *
+ *     RuntimeException is sub class of Exception class
+ *     Anything IS-A RuntimeException is regarded as Unchecked Exception
+ *     We are not forced to handle the exception at compile time.
+ *     Usually happen due to incorrect usage of the method or library
+ *     for example : passing incorrect index for charAt method
+ *     The spoiled ones that we do not have to handle
+ *
+ *     Any Sub class of the Exception class other than RuntimeException is regarded as Checked Exception and we
+ *     MUST (are forced to) handle or declare at compile time
+ *
+ *     Most of the time File related actions are checked exception
+ *     for example : IOException, FileNotFoundException and so on
+ *
+ *     Whats the easiest way if I do not want to google to know its checked or Unchecked Exception ?
+ *     Just try to throw it using throw keyword, if compiler complains, it means its checked Exception
+ *     if not ,then it is unchecked Exception.
+ *
  * Scenario 1::
  * If I ask you tell me your state so I can store :
  *
