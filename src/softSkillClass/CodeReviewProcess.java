44e8565c6a2ae6340bcc645b732cf25f63fa8ad1
Like Sha <tradersalike88@yahoo.com>
2020-11-14 14:19:10 -0800

    QA CodeReviewProcess classnote update

---
+++ src/softSkillClass/CodeReviewProcess.java
@@ -0,0 +1,19 @@
+package softSkillClass;
+
+public class CodeReviewProcess {
+
+    /**
+     * How does code review might happen ? ( how your team doing code review?)
+     *
+     * There are two ways of doing code review :
+     *
+     * 1.we can do it through Github,in Github we have something called " pull request",
+     * how does pull request work is : whenever you want to merge your branch with the master branch,
+     * you have to raise a pull request,whenever you raised a pull request ,we have to have at least
+     * two people approved that pull request within the team. This is the one way the code get
+     * reviewed before you merged.
+     *
+     * 2. second way is : we just get together have a 30 minutes cession with team members,we just go
+     * through all the codes together, then developers say " oh its good "  so our code review done.
+     */
+}
