878d3078ef87532b09aedc37859ec04bbbe6497b
Like Sha <tradersalike88@yahoo.com>
2020-11-14 14:23:19 -0800

    QA DefinitionOfDoneConcept classnote update

---
+++ src/softSkillClass/DefinitionOfDoneConcept.java
@@ -0,0 +1,13 @@
+package softSkillClass;
+
+public class DefinitionOfDoneConcept {
+
+    /**
+     *  1. Unit tests passed
+     *  2.Code reviewed done
+     *  3.Acceptance criteria met
+     *  4.Functional Tests passed
+     *  5.Non-Functional requirement met
+     *  6.Product Owner accepts the User Story
+     */
+}
