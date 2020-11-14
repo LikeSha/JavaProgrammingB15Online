174bb2246da18dd0dd79cf70e4d4ee6d90cc9d95
Like Sha <tradersalike88@yahoo.com>
2020-11-14 14:03:44 -0800

    QA TeamCapacityPoints classnote update

---
+++ src/softSkillClass/TeamCapacityPoints.java
@@ -0,0 +1,27 @@
+package softSkillClass;
+
+public class TeamCapacityPoints {
+
+    /**
+     * I have 8 people in my team.
+     * 4 developers, 2 QA , 1 PO , 1 SM
+     *
+     * How many hours are you doing scrum activities in a day ?
+     * typically 6 hours
+     *
+     * If I have 2 weeks sprint
+     * 10 days x 6 hours = 60 hours <-------My individual capacity.
+     *
+     * (4+2) x 60 = 360 hours <-----Team capacity ( we have 4 dev and 2 QA ,TOTAL 6 PPL)
+     *
+     * In my company 1 point = 10 hours
+     * So how many points worth of story I can complete within this sprint ?
+     * 360/10=36 points.
+     *
+     * Velocity :
+     *
+     * If I work 100% velocity, I will complete 36 points worth of story by end of the sprint.
+     * In real life , we always want to commit little bit more points than our capacity,
+     * typically it's about 10% more , it's going to be roughly 40 points.
+     */
+}
