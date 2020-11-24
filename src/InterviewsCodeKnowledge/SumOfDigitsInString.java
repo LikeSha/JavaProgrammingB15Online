f8d85fff719c1d09bc9873c9ec2377e3d2cd738d
Like Sha <tradersalike88@yahoo.com>
2020-11-23 19:04:44 -0800

    InterviewsCodeKnowledge sum of digits in a String

---
+++ src/InterviewsCodeKnowledge/SumOfDigitsInString.java
@@ -0,0 +1,31 @@
+package InterviewsCodeKnowledge;
+
+public class SumOfDigitsInString {
+
+    // Write a method that can return the sum of the digits in a string
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOfDigits("djojsuinha4478d"));
+
+
+        }
+
+        public static int sumOfDigits(String str){
+
+        char[] allChars = str.toCharArray();
+        int sum = 0;
+
+        for(char each : allChars){
+            if(Character.isDigit(each)){
+                sum+= Integer.valueOf(each+"");
+            }
+        }
+
+        return sum;
+
+    }
+
+
+
+}
