d7c22df563bf8437d8083e5aed82383a427c8a19
Like Sha <tradersalike88@yahoo.com>
2020-12-13 22:43:39 -0800

    InterviewsCodeKnowledge NestedLoopMultiVersion

--- src/InterviewsCodeKnowledge/NestedLoopMultiVersion.java
+++ src/InterviewsCodeKnowledge/NestedLoopMultiVersion.java
@@ -13,6 +13,45 @@ public class NestedLoopMultiVersion {
             }
         }
 
+        // nested for each loop
+
+        System.out.println();
+
+        System.out.println("#########################nested for each loop########################");
+
+        int[] cols = {1,2,3};
+        int[] rows = {1,2,3,4};
+
+        for(int row :rows){
+            System.out.println();
+
+            for(int column : cols){
+                System.out.print("|Row" + row + "-Column " + column);
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("###############nested while loop#####################");
+
+        int row = 0;
+        while(row<5){
+            row++;
+            System.out.println();
+
+            int col = 0;
+            while(col<3){
+                col++;
+                System.out.print("|Row " + row + "-Column " + col);
+            }
+        }
+
+
+
+
+
+
+
 
 
 
