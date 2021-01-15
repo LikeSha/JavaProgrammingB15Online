25ce6d9640cc37afd5fd789c054271247579931b
Like Sha <tradersalike88@yahoo.com>
2021-01-15 12:02:15 -0800

    Interview Questions FindHowManyCharacerInString

---
+++ src/InterviewsCodeKnowledge/FindHowManyCharacerInString.java
@@ -0,0 +1,28 @@
+package InterviewsCodeKnowledge;
+
+public class FindHowManyCharacerInString {
+
+    public static void main(String[] args) {
+
+        // in this case ,we use nested loop
+        String str = "java";
+        String check = "";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if(check.contains(str.charAt(i) + ""))
+                continue;
+
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if(str.charAt(i)==str.charAt(j))
+                    count++;
+
+            }
+            System.out.println(str.charAt(i) + " - " + count);
+            check += str.charAt(i);
+        }
+
+    }
+}
