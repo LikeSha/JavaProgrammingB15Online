ca8c4c0576b95b31d513190b4f49d038cabbf65d
Like Sha <tradersalike88@yahoo.com>
2021-01-16 16:54:50 -0800

    AmstrongNumbers

--- src/InterviewsCodeKnowledge/ArmstrongNumbers.java
+++ src/InterviewsCodeKnowledge/ArmstrongNumbers.java
@@ -7,7 +7,7 @@ public class ArmstrongNumbers {
 
     An Armstrong number of three digits is an integer such that the sum of the cubes
     of its digits is equal to the number itself.
-    For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.
+    For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371--> 3^3 + 7^7 + 1^1
      */
 
     public static void main(String[] args) {
