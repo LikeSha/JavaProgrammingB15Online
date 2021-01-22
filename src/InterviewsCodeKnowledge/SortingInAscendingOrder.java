c7fcdd29180f5d2f0b073e0dd75960a12f5cde4a
Like Sha <tradersalike88@yahoo.com>
2021-01-22 10:56:22 -0800

    SoringArrayListAscendingOrder

--- src/InterviewsCodeKnowledge/SortingInAscendingOrder.java
+++ src/InterviewsCodeKnowledge/SortingInAscendingOrder.java
@@ -8,11 +8,13 @@ public class SortingInAscendingOrder {
 
     public static void main(String[] args) {
 
-
+         // 4,1,4,5,2,3,1
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,2,3,2,3,1));
+        System.out.println(SortingArrayListAsc(list));
 
     }
 
-    public static void SortingArrayListAsc(List<Integer> list){
+    public static ArrayList<Integer> SortingArrayListAsc(ArrayList<Integer> list){
 
         for (int i = 0; i < list.size(); i++) {
             for (int j = 0; j < list.size(); j++) {
@@ -23,5 +25,6 @@ public class SortingInAscendingOrder {
                 }
             }
         }
+        return list;
     }
 }
