b0121e88f4bd7ca0d534d3d00b10822531e7e473
Like Sha <tradersalike88@yahoo.com>
2021-06-24 01:16:12 -0700

    random practice array for loop on day 16 class CountTo10_V2

--- src/day16/CountTo10_V2.java
+++ src/day16/CountTo10_V2.java
@@ -5,7 +5,14 @@ public class CountTo10_V2 {
     public static void main(String[] args) {
 
 
+//         String[] fruits = new String[3];
+//                          fruits[0] = "apples";
+//                          fruits[1] =" oranges";
+//                          fruits[2] = "pears";
 //
+//                          for (int i = 0; i < fruits.length; i++) {
+//                              System.out.println(fruits[i]);
+//                          }
 //       int counter = 1 ;
 //       while (counter <= 10) {
 //
